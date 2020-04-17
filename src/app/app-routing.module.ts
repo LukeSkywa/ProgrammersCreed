dddaa1343fa8d62c04e0ba515007881bfd17058e
naniandrea2000 <andreanani1400@gmail.com>
2020-04-17 16:37:02 +0200

    routing funzionante

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import * as core from '@angular/core';
 
 
 const routes: Routes = [ 
   {path: 'login', component: LoginComponent},
   {path: 'signin', component: RegisterComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+@core.NgModule({
+  imports: [
+      RouterModule.forRoot(routes)
+  ],
+  exports: [
+      RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class RoutingModule{ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -15,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RoutingModule,
     HttpClientModule
   ],
   providers: [],
