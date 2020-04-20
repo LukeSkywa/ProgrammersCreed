b67e1a9a8aa397f8002c65b27609992e3d453441
naniandrea2000 <andreanani1400@gmail.com>
2020-04-20 17:17:37 +0200

    Pipes funzionante

--- src/app/pipes/removewhitespaces.pipe.ts
+++ src/app/pipes/removewhitespaces.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RemovewhitespacesPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, args?: any): string {
+    return value.replace(/ /g, '');
   }
 
 }
