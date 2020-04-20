ed30d41e58ff5c3f19a467b22b9b0b9502c96010
naniandrea2000 <andreanani1400@gmail.com>
2020-04-20 15:12:56 +0200

    Creazione serie interface

---
+++ src/app/models/serie.interface.ts
@@ -0,0 +1,10 @@
+export interface Serie{
+    id:number;
+    path:string;
+    nome:string;
+    numeroStagioni: number;
+    genere: string;
+    descrizione:string;
+    preferiti: boolean;
+    nascosto: boolean;
+}
