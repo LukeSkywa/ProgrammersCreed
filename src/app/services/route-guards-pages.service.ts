2c9ea986533d0c7a54b4643ceede24b4703b7296
naniandrea2000 <andreanani1400@gmail.com>
2020-04-22 12:31:10 +0200

    Creazione route guards per le pagine

---
+++ src/app/services/route-guards-pages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteGuardsPagesService {
+
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (sessionStorage.getItem('user') == null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
