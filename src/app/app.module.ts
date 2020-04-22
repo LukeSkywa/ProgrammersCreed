d23d21d929af91fa41ec4ceecc34f70615984c5d
Lepo00 <luca.leporati@outlook.com>
2020-04-22 09:15:13 +0200

    aggiunta icone list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -41,7 +41,7 @@ import { ModificaprofiloComponent } from './components/modificaprofilo/modificap
     BrowserAnimationsModule,
     HttpClientModule,
     ReactiveFormsModule,
-  
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
