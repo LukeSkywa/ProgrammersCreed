68f2e41e7624e9ad12edc55f12c6c2233d84ee57
naniandrea2000 <andreanani1400@gmail.com>
2020-04-22 12:00:34 +0200

    Aggiunta route guards al routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,17 +8,18 @@ import { FeedbackComponent } from './components/feedback/feedback.component';
 import { ListSerieComponent } from './components/list-serie/list-serie.component';
 import { ModificaprofiloComponent } from './components/modificaprofilo/modificaprofilo.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { RouteGuardsLoginRegisterService } from './services/route-guards-login-register.service';
 
 
 
 const routes: Routes = [ 
   {path: 'login', component: LoginComponent},
   {path: 'signin', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'feedback', component: FeedbackComponent},
-  {path: 'list', component: ListSerieComponent},
-  {path: 'cards',  component:CardsComponent},
-  {path: 'modificaProfilo', component: ModificaprofiloComponent},
+  {path: 'profile', component: ProfileComponent, canActivate: [RouteGuardsLoginRegisterService]},
+  {path: 'feedback', component: FeedbackComponent, canActivate: [RouteGuardsLoginRegisterService]},
+  {path: 'list', component: ListSerieComponent, canActivate: [RouteGuardsLoginRegisterService]},
+  {path: 'cards',  component:CardsComponent, canActivate: [RouteGuardsLoginRegisterService]},
+  {path: 'modificaProfilo', component: ModificaprofiloComponent, canActivate: [RouteGuardsLoginRegisterService]},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
