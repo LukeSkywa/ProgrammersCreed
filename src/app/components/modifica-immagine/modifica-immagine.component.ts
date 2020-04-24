6fdf5c07072a47a537152b669ff5945fabb09dad
naniandrea2000 <andreanani1400@gmail.com>
2020-04-24 14:13:58 +0200

    Creazione array immagini nel ts

--- src/app/components/modifica-immagine/modifica-immagine.component.ts
+++ src/app/components/modifica-immagine/modifica-immagine.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModificaImmagineComponent implements OnInit {
 
+  immagini: string[]=[
+    "../assets/profile/messi.jpg",
+    "../assets/profile/ronaldo.jpg",
+    "../assets/profile/candreva.jpg",
+    "../assets/profile/nagatomo.jpg",
+    "../assets/profile/allegri.jpg",
+    "../assets/profile/hernandez.jpg",
+    "../assets/profile/balotelli.jpg",
+    "../assets/profile/buffon.jpg",
+    "../assets/profile/simeone.jpg",
+    "../assets/profile/ibra.jpg",
+    "../assets/profile/neymar.jpg",
+    "../assets/profile/mazzarri.jpg",
+    "../assets/profile/mourinho.jpg",
+    "../assets/profile/juve.jpg"
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
