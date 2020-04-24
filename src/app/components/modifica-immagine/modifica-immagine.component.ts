5d16647bcbb25b71e55356e3577409850387245e
naniandrea2000 <andreanani1400@gmail.com>
2020-04-24 14:47:34 +0200

    Cambia immagine profilo con link funzionante

--- src/app/components/modifica-immagine/modifica-immagine.component.ts
+++ src/app/components/modifica-immagine/modifica-immagine.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MyHttpService } from 'src/app/services/my-http.service';
 import { User } from 'src/app/models/user.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modifica-immagine',
@@ -28,10 +29,14 @@ export class ModificaImmagineComponent implements OnInit {
   ];
   
   myProfile:User;
+  modificaImg: FormGroup;
 
-  constructor(private router:Router, private myHttpService: MyHttpService) { 
+  constructor(private fb: FormBuilder,private router:Router, private myHttpService: MyHttpService) { 
     this.myHttpService.getMyProfile().subscribe(reponse => {
       this.myProfile = reponse;
+    });
+    this.modificaImg = this.fb.group({
+      immagine:['', Validators.required],
     });
     
   }
@@ -41,6 +46,13 @@ export class ModificaImmagineComponent implements OnInit {
 
   modificaImmagine(immagine:string){
     this.myProfile.immagine=immagine;
+    this.myHttpService.putUser(this.myProfile).subscribe(()=>{
+      this.router.navigateByUrl('profile');
+    });
+  }
+
+  modificaImmagineLink(immagine){
+    this.myProfile.immagine=immagine.value.immagine;
     this.myHttpService.putUser(this.myProfile).subscribe(()=>{
       this.router.navigateByUrl('profile');
     });
